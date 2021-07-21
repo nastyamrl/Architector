e7be83b406aa1f3865b47b7db846ec5362b43431
nastyamrl <nastya.mrl@mail.ru>
2021-07-21 22:11:46 +0300

    third commit

---
+++ js/main.js
@@ -0,0 +1,49 @@
+/*"use strict"
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('form');
+    form.addEventListener('submit' , formsend);
+
+    async function formSend(e) {
+        e.preventDefault();
+        let error = formValidate(form);
+    }
+
+function formValidate(form) {
+    let arror = 0;
+    let formReq = document.querySelectorAll('req');
+    for (let index = 0; index < formReq.length; index++) {
+        const input = formReq[index];
+        formRemoveError (input);
+
+        if(input.classList.contsins("email")) {
+            if(emailTest(input)) {
+                formAddError(input);
+                error++;
+            }
+        } else if(input.getAttribute("type") ==="checkbox" && input.checked ===false){
+            formAddError(input);
+                error++;
+        } else {
+            if(input.value === '') {
+                formAddError(input);
+                error++
+            }
+        }
+
+    }
+}
+function formAddError(input) {
+    input.parentElement.classList.add('arror');
+    input.classList.add('arror');
+}
+function formRemoveError(input) {
+    input.parentElement.classList.remove('arror');
+    input.classList.remove('arror');
+}
+function emailTest (input) {
+    return !/^\w+([\.-]?\w+)*@\w+([\/-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+}
+
+});*/
+
+let reg =
